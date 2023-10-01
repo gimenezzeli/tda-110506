785bf7bd9b988e8e2fe56d27d776c4838a772cbf
gimenezzeli <121507071+gimenezzeli@users.noreply.github.com>
2023-10-01 19:48:12 -0300

    entrega tda

--- README.md
+++ README.md
@@ -2,59 +2,124 @@
 <img width="32px" src="img/algo2.svg">
 </div>
 
-# NOMBRE TP/TDA
+# TP/TDA
 
-## Repositorio de (Nombre Apellido) - (Padrón) - (Mail)
+## Repositorio de Marlene Elizabeth Gimenez Gauto - 110506 - gimenezzeli@gmail.com
 
 - Para compilar:
 
 ```bash
-línea de compilación
+gcc src/*.c --leak-check=full --track-origins=yes --show-reachable=yes --error-exitcode=2 --show-leak-kinds=all --trace-children=yes -std=c99 -Wall -Wconversion -Wtype-limits -pedantic -Werror -O2 -g -o lista
 ```
 
 - Para ejecutar:
 
 ```bash
-línea de ejecución
+./lista
 ```
 
 - Para ejecutar con valgrind:
 ```bash
-línea con valgrind
+make valgrind-chanutron
 ```
 ---
 ##  Funcionamiento
 
-Explicación de cómo funcionan las estructuras desarrolladas en el TP y el funcionamiento general del mismo.
+Mi struct lista contiene un puntero al nodo_inicio este nos sirve como punto de entrada para acceder a los elementos de la lista, un puntero al nodo_fin que nos sirve para achicar la complejidad algoritmica de algunas funciones y una variable cantidad para saber cuantos elementos hay en mi lista.
 
-Aclarar en esta parte todas las decisiones que se tomaron al realizar el TP, cosas que no se aclaren en el enunciado, fragmentos de código que necesiten explicación extra, etc.
-
-Incluír **EN TODOS LOS TPS** los diagramas relevantes al problema (mayormente diagramas de memoria para explicar las estructuras, pero se pueden utilizar otros diagramas si es necesario).
-
-### Por ejemplo:
-
-El programa funciona abriendo el archivo pasado como parámetro y leyendolo línea por línea. Por cada línea crea un registro e intenta agregarlo al vector. La función de lectura intenta leer todo el archivo o hasta encontrar el primer error. Devuelve un vector con todos los registros creados.
-
-<div align="center">
-<img width="70%" src="img/diagrama1.svg">
-</div>
-
-En el archivo `sarasa.c` la función `funcion1` utiliza `realloc` para agrandar la zona de memoria utilizada para conquistar el mundo. El resultado de `realloc` lo guardo en una variable auxiliar para no perder el puntero original en caso de error:
+Empiezo creando una funcion para crear una lista usando malloc y verifico si la memoria se guardo correctamente, si fue asi devuelvo una lista vacia.
 
 ```c
-int *vector = realloc(vector_original, (n+1)*sizeof(int));
+lista_t *lista_crear(){
+	lista_t *nueva_lista = malloc(sizeof(lista_t));
+	if (!nueva_lista){
+		free(nueva_lista);
+		return NULL;
+	}
+	nueva_lista->cantidad = 0;
+	nueva_lista->nodo_inicio = NULL;
 
-if(vector == NULL)
-    return -1;
-vector_original = vector;
+	return nueva_lista;
+}
+```
+Como voy a necesitar muchos nodos, me cree una funcion que recibe un elemento el cual me sirve para crear un nodo usando malloc y verifico si la memoria se guardo correctamente, si fue asi devuelvo un nodo.  
+
+```c
+nodo_t *crear_nodo(void *elemento){
+	nodo_t *nuevo_nodo = malloc(sizeof(nodo_t));
+
+	if(!nuevo_nodo){
+		free(nuevo_nodo);
+		return NULL;
+	}
+
+	nuevo_nodo->elemento = elemento;
+	nuevo_nodo->siguiente = NULL;
+
+	return nuevo_nodo;
+}
+
 ```
 
+Si quiero insertar al final, hago las verificaciones correspondientes:
+Caso especial: si la lista esta vacia 
+Hago que el nodo_inicio y el nodo_fin apunten al nuevo_nodo previamente creado. Y aumento la cantidad de elementos +1. Al finalizar devuelvo la lista.
+Otro caso: si la lista no esta vacia
+Si  la lista ya tiene nodos dentro hago que el nodo_fin->siguiente apunte al nuevo nodo y cambio el nodo_fin al nuevo_nodo insertado. Y aumento la cantidad de elementos +1. Al finalizar devuelvo la lista.
 
-<div align="center">
-<img width="70%" src="img/diagrama2.svg">
-</div>
+Si quiero insertar en una posicion en especifico, hago las verificaciones correspondientes (entre ellas esta que si la lista esta vacia o que si la posicion en la que quiero insertar es mayor al tamaño de la lista, el nuevo nodo se insertara al final). 
+Caso especial: si la posicion en la que quiero insertar es la posicion 0
+Hago que el nuevo_nodo->siguiente apunte al nodo_inicio y cambio el nodo_inicio al nuevo_nodo. Y aumento la cantidad de elementos +1. Al finalizar devuelvo la lista.
+Otro caso: si la posicion en la que quiero insertar NO es la primera.
+Tengo que recorrer la lista hasta que mi contador (previamente inicializado en 0) sea igual a la posicion dada menos 1, ya que las pociones de a la lista arrancan en 0. Una vez encontrada la posicion hago que el nuevo_nodo->siguiente apunte al nodo_actual->siguiente y despues hago que el nodo_actual->siguiente apunte al nuevo nodo. Aumento la cantidad de elementos +1. Al finalizar devuelvo la lista.
+
+Si quiero eliminar al final, hago las verificaciones correspondientes:
+Caso especial:si la lista solo tiene un elemento
+Libero al nodo_inicio y hago que el nodo_inicio y el nodo_fin apunten a NULL, despues libero mi nodo_viejo que es un auxiliar. Disminuyo la cantidad de elementos que tiene mi lista -1. 
+Otro caso: si l lista tiene mas de un elemento 
+Tengo que recorrer la lista hasta llegar al penúltimo nodo, utilizo el nodo_viejo para guardarme el penultimo nodo y nodo_actual para guardarme el ultimo nodo. Me guardo el elemento del ultimo nodo para devolverlo al finalizar. Despues hago que el penultimo nodo es decir el nodo_viejo apunte a NULL y cambio el nodo_fin y apunta al nodo_viejo que va a ser nuestro nuevo fin. Libero el nodo y reduzco el tamaño de la lista. Al finalizar devuelvo el elemento del nodo borrado.
+
+Si quiero eliminar de una posicion en especifico, hago las verificaciones correspondientes:
+Caso especial: si la posicion a eliminar es la 0
+Hago que el nodo_inicio apunte al nodo_inicio->siguiente; libero los nodos auxiliares, reduzco el tamaño de la lista y devuelvo el elemento del nodo borrado que previamente me guarde.
+Caso especial: si la posicion es mayor o igual al tamaño de la lista
+Debo eliminar al ultimo, puedo llamar a la funcion lista_quitar que elimina el ultimo y me devuelve el elemento del nodo borrado y este lo guardo en una variable y lo devuelvo al finalizar.
+Otro caso: eliminar en una posicion valida
+Tengo que recorrer la lista hasta que mi contador (previamente inicializado en 0) sea menor a la posicion dada. Una vez que encontre el nodo de la posicion utilizo el nodo_viejo para guardarme el penultimo nodo y nodo_actual para guardarme el nodo de la posicion. Actulizo el puntero del nodo anterior. Digo que el nodo_viejo->siguiente apunte al nodo_actual->siguiente y nodo_actual->siguiente apunte a NULL. Libero mi nodo_actual, reduzco el tamaño de la lista y devuelvo el elemento del nodo borrado que anteriormente me guarde en una variable.
+
+Si busco un elemento en una posicion dada, debo recorrer mi lista hasta que mi contador inicializado en 0 sea menor a la posicion dada; una vez ahi estoy en el nodo correcto. Devuelvo el elemento de ese nodo.
 
 ---
 
 ## Respuestas a las preguntas teóricas
-Incluír acá las respuestas a las preguntas del enunciado (si aplica).
+ - ¿Qué es una lista/pila/cola? Explicar con diagramas.
+<div align="center">
+<img width="70%" src="img/lista.png">
+</div>
+<div align="center">
+<img width="70%" src="img/pila.png">
+</div>
+<div align="center">
+<img width="70%" src="img/cola.png">
+</div>
+
+ - Explica y analiza las diferencias de complejidad entre las implementaciones de lista simplemente enlazada, doblemente enlazada y vector dinámico para las operaciones:
+   - Insertar/obtener/eliminar al inicio
+   - Insertar/obtener/eliminar al final
+   - Insertar/obtener/eliminar al medio
+
+   Vector dinamico:
+    -  Insertar/obtener/eliminar al inicio O(n)
+    - Insertar/obtener/eliminar al final O(n)
+    - Insertar/obtener/eliminar al medio O(n)
+    Lista enlazada:
+    -  Insertar/obtener/eliminar al inicio O(1)
+    - Insertar/obtener/eliminar al final O(n)
+    - Insertar/obtener/eliminar al medio O(n)
+
+
+- Explica la complejidad de las operaciones implementadas en tu trabajo para la pila y la cola.
+    Push y Pop: O(1)
+    Ya que 'convierto' mi inicio en el final de mi vector, voy agregando siempre al principio que es O(1) y tambien voy eliminando al primer elemento de mi lista, que en realidad es el ultimo que entro q tambien es O(1)
+    Encolar y desencolar: O(1)
+    Como tengo un puntero a mi nodo_fin inserto al final siendo este O(1), y desencolar el primer elemento de la lista tambien es O(1)
--- pruebas_alumno.c
+++ pruebas_alumno.c
@@ -1,17 +1,583 @@
+#include "./src/lista.h"
 #include "pa2m.h"
+#include <stdlib.h>
 
-void prueba_simple()
+void crear_lista()
 {
-	int i = 14;
-	int j = 16;
-	pa2m_afirmar(i != j, "i=14 es diferente de j=16");
+	lista_t *lista = lista_crear();
+	pa2m_afirmar(lista != NULL, "Una lista creada es != NULL");
+	pa2m_afirmar(lista_tamanio(lista) == 0, "La lista creada esta vacia");
+	lista_destruir(lista);
+}
+
+void insertar_agranda_la_lista()
+{
+	lista_t *lista = lista_crear();
+	void *e1 = (void *)0xbebecafe;
+	lista_insertar(lista, e1);
+	pa2m_afirmar(lista_tamanio(lista) == 1, "Puedo insertar un elemento.");
+	pa2m_afirmar(lista_tamanio(lista) == 1,
+		     "El tamaño de la lista es el correcto.");
+	void *e2 = (void *)0xbebecaff;
+	lista_insertar(lista, e2);
+	void *e3 = (void *)0xbebecafa;
+	lista_insertar(lista, e3);
+	void *e4 = (void *)0xbebecafb;
+	lista_insertar(lista, e4);
+	void *e5 = (void *)0xbebecafc;
+	lista_insertar(lista, e5);
+	pa2m_afirmar(lista_tamanio(lista) == 5,
+		     "Puedo insertar mas elementos.");
+	pa2m_afirmar(lista_tamanio(lista) == 5,
+		     "El tamaño de la lista es el correcto.");
+	lista_destruir(lista);
+}
+
+void comprobar_posiciones()
+{
+	lista_t *lista = lista_crear();
+	void *e1 = (void *)0xbebecafe;
+	lista_insertar(lista, e1);
+	void *e2 = (void *)0xbebecaff;
+	lista_insertar(lista, e2);
+	void *e3 = (void *)0xbebecafa;
+	lista_insertar(lista, e3);
+	void *e4 = (void *)0xbebecafb;
+	lista_insertar(lista, e4);
+	pa2m_afirmar(
+		lista_elemento_en_posicion(lista, 0) == e1,
+		"La posicion del primer elemento de la lista es el correcto.");
+	pa2m_afirmar(lista_elemento_en_posicion(lista, 1) == e2,
+		     "La posicion del 2° elemento de la lista es el correcto.");
+	pa2m_afirmar(lista_elemento_en_posicion(lista, 2) == e3,
+		     "La posicion del 3° elemento de la lista es el correcto.");
+	pa2m_afirmar(
+		lista_elemento_en_posicion(lista, 3) == e4,
+		"El ultimo elemento insertado esta en la posicion correcta.");
+	lista_destruir(lista);
+}
+
+void insertar_en_lista_nula()
+{
+	lista_t *lista_nula = NULL;
+	void *e1 = (void *)0xbeaecafe;
+	lista_t *lista_nueva = lista_insertar(lista_nula, e1);
+	pa2m_afirmar(lista_nueva == NULL,
+		     "No inserta al final en una lista nula.");
+	lista_t *lista_nueva2 = lista_insertar_en_posicion(lista_nula, e1, 10);
+	pa2m_afirmar(lista_nueva2 == NULL,
+		     "No inserta en ninguna posicion si es una lista nula.");
+	lista_destruir(lista_nula);
+}
+
+void insertar_al_final()
+{
+	lista_t *lista = lista_crear();
+	void *e1 = (void *)0xbebecafe;
+	lista_insertar(lista, e1);
+	pa2m_afirmar(lista_elemento_en_posicion(lista, 0) == e1,
+		     "El elemento insertado esta en la posicion correcta.");
+	void *e2 = NULL;
+	lista_insertar(lista, e2);
+	pa2m_afirmar(
+		lista_elemento_en_posicion(lista, 1) == e2,
+		"El elemento NULL insertado esta en la posicion correcta.");
+	lista_insertar(lista, e1);
+	pa2m_afirmar(lista_elemento_en_posicion(lista, 2) ==
+			     lista_elemento_en_posicion(lista, 0),
+		     "Puedo insertar un elemento repetido.");
+	lista_destruir(lista);
+}
+
+void insertar_en_posicion()
+{
+	lista_t *lista = lista_crear();
+	void *e1 = (void *)0xbebecafe;
+	lista_insertar_en_posicion(lista, e1, 100);
+	pa2m_afirmar(
+		lista_elemento_en_posicion(lista, 0) == e1,
+		"El elemento insertado en la posicion 100 esta en la posicion correcta (al principio).");
+	void *e2 = (void *)0xbebecafa;
+	lista_insertar(lista, e2);
+	void *e3 = (void *)0xbebecafb;
+	lista_insertar(lista, e3);
+	void *e4 = (void *)0xbebecafc;
+	lista_insertar_en_posicion(lista, e4, 15);
+	pa2m_afirmar(
+		lista_elemento_en_posicion(lista, 3) == e4,
+		"El elemento insertado en la posicion 15 esta en la posicion correcta (al final).");
+	lista_destruir(lista);
+}
+
+void eliminar_achica_la_lista()
+{
+	lista_t *lista = lista_crear();
+	void *e1 = (void *)0xbebecafe;
+	lista_insertar(lista, e1);
+	void *e2 = (void *)0xbebecaff;
+	lista_insertar(lista, e2);
+	void *e3 = (void *)0xbebecafd;
+	lista_insertar(lista, e3);
+	void *e4 = (void *)0xbebecaef;
+	lista_insertar(lista, e4);
+	lista_quitar(lista);
+	pa2m_afirmar(lista_tamanio(lista) == 3,
+		     "Puedo eliminar un elemento de la lista.");
+	pa2m_afirmar(lista_tamanio(lista) == 3,
+		     "El tamaño de la lista se achica.");
+	lista_quitar(lista);
+	pa2m_afirmar(lista_tamanio(lista) == 2,
+		     "Puedo eliminar otro elemento de la lista.");
+	pa2m_afirmar(lista_tamanio(lista) == 2,
+		     "El tamaño de la lista se achica.");
+	lista_destruir(lista);
+}
+
+void eliminar_elementos()
+{
+	lista_t *lista = lista_crear();
+	void *e1 = (void *)0xbebecafe;
+	lista_insertar(lista, e1);
+	void *e2 = (void *)0xbebecaff;
+	lista_insertar(lista, e2);
+	void *e3 = (void *)0xbebecafd;
+	lista_insertar(lista, e3);
+	void *e4 = (void *)0xbebecaef;
+	lista_insertar(lista, e4);
+	void *elemento_borrado1 = lista_quitar_de_posicion(lista, 1);
+	pa2m_afirmar(elemento_borrado1 == e2,
+		     "El elemento borrado de la posicion 1 es el correcto.");
+	void *elemento_borrado2 = lista_quitar(lista);
+	pa2m_afirmar(elemento_borrado2 == e4,
+		     "El ultimo elemento borrado es el correcto.");
+	lista_destruir(lista);
+}
+
+void quitar_elementos_de_lista_nula()
+{
+	lista_t *lista = NULL;
+	void *elemento_borrado1 = lista_quitar(lista);
+	void *elemento_borrado2 = lista_quitar_de_posicion(lista, 12);
+	pa2m_afirmar(elemento_borrado1 == NULL,
+		     "No puedo borrar el ultimo elemento de una lista nula.");
+	pa2m_afirmar(
+		elemento_borrado2 == NULL,
+		"No puedo borrar un elemento de una posicion arbitraria de una lista nula.");
+	lista_destruir(lista);
+}
+
+void quitar_elementos_de_lista_vacia()
+{
+	lista_t *lista = lista_crear();
+	void *elemento_borrado1 = lista_quitar(lista);
+	void *elemento_borrado2 = lista_quitar_de_posicion(lista, 12);
+	pa2m_afirmar(elemento_borrado1 == NULL,
+		     "No puedo borrar el ultimo elemento de una lista vacia.");
+	pa2m_afirmar(
+		elemento_borrado2 == NULL,
+		"No puedo borrar un elemento de una posicion arbitraria de una lista vacia.");
+	lista_destruir(lista);
+}
+
+void multiple_eliminacion()
+{
+	lista_t *lista = lista_crear();
+	void *e1 = (void *)0xbebecafe;
+	lista_insertar(lista, e1);
+	void *e2 = (void *)0xbebecaff;
+	lista_insertar(lista, e2);
+	void *e3 = (void *)0xbebecafa;
+	lista_insertar(lista, e3);
+	void *e4 = (void *)0xbebecafb;
+	lista_insertar(lista, e4);
+	void *e5 = (void *)0xbebecafc;
+	lista_insertar(lista, e5);
+	lista_quitar(lista);
+	lista_quitar(lista);
+	lista_quitar(lista);
+	lista_quitar(lista);
+	pa2m_afirmar(lista_tamanio(lista) == 1,
+		     "Se eliminaron varios elementos de la lista.");
+	void *e6 = (void *)0xbebecaac;
+	lista_insertar(lista, e6);
+	void *elemento_borrado1 = lista_quitar_de_posicion(lista, 100);
+	pa2m_afirmar(
+		elemento_borrado1 == e6,
+		"Se elimino el ultimo elemento de la lista cuando se le pasa una posicion inexistente.");
+	void *e7 = NULL;
+	lista_insertar(lista, e7);
+	void *elemento_borrado2 = lista_quitar(lista);
+	pa2m_afirmar(elemento_borrado2 == e7,
+		     "Se elimino el elemento NULL de la lista.");
+	lista_destruir(lista);
+}
+
+void obtener_elementos_lista_nula()
+{
+	lista_t *lista_nula = NULL;
+	void *elemento1 = lista_elemento_en_posicion(lista_nula, 5);
+	pa2m_afirmar(elemento1 == NULL,
+		     "No se puede obtener un elemento de una lista nula.");
+	lista_destruir(lista_nula);
+}
+
+void obtener_elementos_lista_vacia()
+{
+	lista_t *lista_vacia = lista_crear();
+	void *elemento2 = lista_elemento_en_posicion(lista_vacia, 3);
+	pa2m_afirmar(elemento2 == NULL,
+		     "No se puede obtener un elemento de una lista vacia.");
+	lista_destruir(lista_vacia);
+}
+
+void obtener_elementos()
+{
+	lista_t *lista = lista_crear();
+	void *e1 = (void *)0xbebecaff;
+	lista_insertar(lista, e1);
+	void *e2 = (void *)0xbebecafa;
+	lista_insertar(lista, e2);
+	void *e3 = (void *)0xbebecafb;
+	lista_insertar(lista, e3);
+	void *e4 = (void *)0xbebecafc;
+	lista_insertar(lista, e4);
+	void *elemento1 = lista_elemento_en_posicion(lista, 10);
+	pa2m_afirmar(
+		elemento1 == NULL,
+		"No se puede obtener un elemento de posicion inexistente.");
+	void *elemento2 = lista_elemento_en_posicion(lista, 2);
+	pa2m_afirmar(elemento2 == e3,
+		     "El elemento en posicion 2 es el correcto.");
+	lista_destruir(lista);
+}
+
+void conteo_lista_nula()
+{
+	lista_t *lista_nula = NULL;
+	pa2m_afirmar(lista_tamanio(lista_nula) == 0,
+		     "La cantidad de elementos de la lista nula es 0.");
+	pa2m_afirmar(lista_vacia(lista_nula), "La lista nula esta vacia.");
+	lista_destruir(lista_nula);
+}
+
+void conteo_lista_vacia()
+{
+	lista_t *lista = NULL;
+	pa2m_afirmar(lista_tamanio(lista) == 0,
+		     "La cantidad de elementos de la lista vacia es 0.");
+	pa2m_afirmar(lista_vacia(lista), "La lista vacia esta vacia.");
+	lista_destruir(lista);
+}
+
+void conteo_lista()
+{
+	lista_t *lista = lista_crear();
+	void *e1 = (void *)0xbebecafe;
+	lista_insertar(lista, e1);
+	pa2m_afirmar(lista_tamanio(lista) == 1,
+		     "El tamaño de la lista es el correcto.");
+	void *e2 = (void *)0xbebecaff;
+	lista_insertar(lista, e2);
+	pa2m_afirmar(lista_tamanio(lista) == 2,
+		     "El tamaño de la lista es el correcto.");
+	void *e3 = (void *)0xbebecafa;
+	lista_insertar(lista, e3);
+	pa2m_afirmar(lista_tamanio(lista) == 3,
+		     "El tamaño de la lista es el correcto.");
+	void *e4 = (void *)0xbebecafb;
+	lista_insertar(lista, e4);
+	pa2m_afirmar(lista_tamanio(lista) == 4,
+		     "El tamaño de la lista es el correcto.");
+	void *e5 = (void *)0xbebecafc;
+	lista_insertar(lista, e5);
+	pa2m_afirmar(lista_tamanio(lista) == 5,
+		     "El tamaño de la lista es el correcto.");
+	pa2m_afirmar(!lista_vacia(lista),
+		     "Una lista con elementos no esta vacia");
+	lista_destruir(lista);
+}
+
+int elemento_es_igual_a(void *_a, void *_b)
+{
+	void *a = _a;
+	void *b = _b;
+
+	if (a && b && a == b)
+		return 0;
+
+	return -1;
+}
+
+void buscar_en_lista_nula()
+{
+	lista_t *lista_nula = NULL;
+	void *e1 = (void *)0xbebecafc;
+	void *elemento2 =
+		lista_buscar_elemento(lista_nula, elemento_es_igual_a, e1);
+	pa2m_afirmar(elemento2 == NULL,
+		     "No se puede buscar en una lista nula.");
+	lista_destruir(lista_nula);
+}
+
+void buscar_en_lista_vacia()
+{
+	lista_t *lista = lista_crear();
+	void *e1 = (void *)0xbebecafc;
+	void *elemento1 = lista_buscar_elemento(lista, elemento_es_igual_a, e1);
+	pa2m_afirmar(elemento1 == NULL,
+		     "No se puede buscar en una lista vacia.");
+	lista_destruir(lista);
+}
+
+void buscar_lista_comparador_nulo()
+{
+	lista_t *lista = lista_crear();
+	void *e1 = (void *)0xbebecafc;
+	lista_insertar(lista, e1);
+	void *e2 = (void *)0xbebecaec;
+	lista_insertar(lista, e2);
+	void *e3 = (void *)0xbebecabc;
+	lista_insertar(lista, e3);
+	void *elemento1 = lista_buscar_elemento(lista, NULL, e1);
+	pa2m_afirmar(elemento1 == NULL, "No puedo buscar con comparador nulo.");
+	lista_destruir(lista);
+}
+
+void buscar_lista_contexto_nulo()
+{
+	lista_t *lista = lista_crear();
+	void *e1 = (void *)0xbebecafc;
+	lista_insertar(lista, e1);
+	void *e2 = (void *)0xbebecaec;
+	lista_insertar(lista, e2);
+	void *e3 = (void *)0xbebecabc;
+	lista_insertar(lista, e3);
+	void *elemento1 =
+		lista_buscar_elemento(lista, elemento_es_igual_a, NULL);
+	pa2m_afirmar(elemento1 == NULL, "No puedo buscar con contexto nulo.");
+	lista_destruir(lista);
+}
+
+void buscar_lista_elemento_no_existente()
+{
+	lista_t *lista = lista_crear();
+	void *e1 = (void *)0xbebecafc;
+	lista_insertar(lista, e1);
+	void *e2 = (void *)0xbebecaec;
+	lista_insertar(lista, e2);
+	void *e3 = (void *)0xbebecabc;
+	lista_insertar(lista, e3);
+	void *e4 = (void *)0xbebecebc;
+	void *elemento1 = lista_buscar_elemento(lista, elemento_es_igual_a, e4);
+	pa2m_afirmar(elemento1 == NULL,
+		     "No se puede buscar un elemento inexistente.");
+	lista_destruir(lista);
+}
+
+void buscar_lista_elemento()
+{
+	lista_t *lista = lista_crear();
+	void *e1 = (void *)'A';
+	lista_insertar(lista, e1);
+	void *e2 = (void *)'B';
+	lista_insertar(lista, e2);
+	void *e3 = (void *)'C';
+	lista_insertar(lista, e3);
+	void *elemento1 = lista_buscar_elemento(lista, elemento_es_igual_a, e3);
+	pa2m_afirmar(elemento1 == e3, "El elemento buscado es el correcto.");
+	lista_destruir(lista);
+}
+
+void buscar_lista_elemento_duplicado()
+{
+	lista_t *lista = lista_crear();
+	void *e1 = (void *)'A';
+	lista_insertar(lista, e1);
+	lista_insertar(lista, e1);
+	void *e2 = (void *)'B';
+	lista_insertar(lista, e2);
+	void *elemento1 = lista_buscar_elemento(lista, elemento_es_igual_a, e1);
+	pa2m_afirmar(elemento1 == e1, "Se puede buscar un elemento repetido.");
+	lista_destruir(lista);
+}
+
+void buscar_primer_elemento_en_lista_nula()
+{
+	lista_t *lista_nula = NULL;
+	void *elemento = lista_primero(lista_nula);
+	pa2m_afirmar(
+		elemento == NULL,
+		"No se puede obtener el primer elemento de una lista nula.");
+	lista_destruir(lista_nula);
+}
+
+void buscar_ultimo_elemento_en_lista_nula()
+{
+	lista_t *lista_nula = NULL;
+	void *elemento = lista_elemento_en_posicion(
+		lista_nula, lista_tamanio(lista_nula) - 1);
+	pa2m_afirmar(
+		elemento == NULL,
+		"No se puede obtener el ultimo elemento de una lista nula.");
+	lista_destruir(lista_nula);
+}
+
+void buscar_primer_elemento_en_lista_vacia()
+{
+	lista_t *lista = lista_crear();
+	void *elemento = lista_primero(lista);
+	pa2m_afirmar(
+		elemento == NULL,
+		"No se puede obtener el primer elemento de una lista vacia.");
+	lista_destruir(lista);
+}
+
+void buscar_ultimo_elemento_en_lista_vacia()
+{
+	lista_t *lista = lista_crear();
+	void *elemento =
+		lista_elemento_en_posicion(lista, lista_tamanio(lista) - 1);
+	pa2m_afirmar(
+		elemento == NULL,
+		"No se puede obtener el ultimo elemento de una lista vacia.");
+	lista_destruir(lista);
+}
+
+void buscar_primer_elemento()
+{
+	lista_t *lista = lista_crear();
+	void *e1 = (void *)'A';
+	lista_insertar(lista, e1);
+	void *e2 = (void *)'B';
+	lista_insertar(lista, e2);
+	void *e3 = (void *)'C';
+	lista_insertar(lista, e3);
+	void *elemento = lista_primero(lista);
+	pa2m_afirmar(elemento == e1,
+		     "El elemento de la primer posicion es el correcto.");
+	lista_destruir(lista);
+}
+
+void buscar_ultimo_elemento()
+{
+	lista_t *lista = lista_crear();
+	void *e1 = (void *)'A';
+	lista_insertar(lista, e1);
+	void *e2 = (void *)'B';
+	lista_insertar(lista, e2);
+	void *e3 = (void *)'C';
+	lista_insertar(lista, e3);
+	void *elemento =
+		lista_elemento_en_posicion(lista, lista_tamanio(lista) - 1);
+	pa2m_afirmar(elemento == e3,
+		     "El elemento de la ultima posicion es el correcto.");
+	lista_destruir(lista);
+}
+
+void pruebas_de_primero_y_ultimo()
+{
+	buscar_primer_elemento_en_lista_nula();
+	buscar_ultimo_elemento_en_lista_nula();
+	buscar_primer_elemento_en_lista_vacia();
+	buscar_ultimo_elemento();
+	buscar_primer_elemento();
+	buscar_ultimo_elemento();
+}
+
+void iteradores()
+{
+	lista_iterador_t *it = NULL;
+	pa2m_afirmar(it == NULL, "Se puede crear un iterador nulo.");
+	lista_iterador_destruir(it);
+}
+
+void pruebas_de_insertar()
+{
+	insertar_agranda_la_lista();
+	printf("\n");
+	insertar_al_final();
+	printf("\n");
+	comprobar_posiciones();
+	printf("\n");
+	insertar_en_lista_nula();
+	printf("\n");
+	insertar_en_posicion();
+}
+
+void pruebas_de_eliminar()
+{
+	eliminar_achica_la_lista();
+	printf("\n");
+	eliminar_elementos();
+	printf("\n");
+	quitar_elementos_de_lista_nula();
+	printf("\n");
+	quitar_elementos_de_lista_vacia();
+	printf("\n");
+	multiple_eliminacion();
+}
+
+void pruebas_de_obtencion()
+{
+	obtener_elementos_lista_nula();
+	printf("\n");
+	obtener_elementos_lista_vacia();
+	printf("\n");
+	obtener_elementos();
+}
+
+void pruebas_de_cantidad()
+{
+	conteo_lista_nula();
+	conteo_lista_vacia();
+	printf("\n");
+	conteo_lista();
+}
+
+void pruebas_de_buscar()
+{
+	buscar_en_lista_nula();
+	buscar_en_lista_vacia();
+	buscar_lista_comparador_nulo();
+	buscar_lista_contexto_nulo();
+	buscar_lista_elemento_no_existente();
+	buscar_lista_elemento();
+	buscar_lista_elemento_duplicado();
+	printf("\n");
+	pruebas_de_primero_y_ultimo();
+}
+
+void destruir_lista()
+{
+	lista_t *lista = lista_crear();
+
+	lista_insertar(lista, malloc(sizeof(int)));
+	lista_insertar(lista, malloc(sizeof(int)));
+	lista_insertar(lista, malloc(sizeof(int)));
+	lista_insertar(lista, malloc(sizeof(int)));
+	lista_insertar(lista, malloc(sizeof(int)));
+
+	lista_destruir_todo(lista, free);
 }
 
 int main()
 {
 	pa2m_nuevo_grupo(
-		"\n======================== XXX ========================");
-	prueba_simple();
+		"\n======================== MIS PRUEBAS ========================");
+
+	pa2m_nuevo_grupo("\nPruebas de crear");
+	crear_lista();
+	pa2m_nuevo_grupo("\nPruebas de insertar");
+	pruebas_de_insertar();
+	pa2m_nuevo_grupo("\nPruebas de eliminar");
+	pruebas_de_eliminar();
+	pa2m_nuevo_grupo("\nPruebas de obtencion");
+	pruebas_de_obtencion();
+	pa2m_nuevo_grupo("\nPruebas de cantidad");
+	pruebas_de_cantidad();
+	pa2m_nuevo_grupo("\nPruebas de buscar");
+	pruebas_de_buscar();
+	destruir_lista();
 
 	return pa2m_mostrar_reporte();
 }
